32f9ffa23882c7f23aac12e983a04240188833d3
Nick Kullman <Nick.Kullman@gmail.com>
2016-04-11 14:45:37 -0700

    Add empty interface for ADAlgo interfaces
    What must all interfaces for the alpha-delta algorithm do?

---
+++ src/mco/alphadelta/framework/IADAlgoInterface.java
@@ -0,0 +1,13 @@
+package mco.alphadelta.framework;
+
+/**
+ * Defines the interface for interfacing with the alpha-delta algorithm.
+ * 
+ * @author nkullman
+ * @version %I%, %G%
+ * @since April 11, 2016
+ */
+
+public interface IADAlgoInterface {
+
+}
