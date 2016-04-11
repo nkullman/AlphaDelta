dac05e9eb3dda59a327a687dd3d54b7367cf3637
Nick Kullman <Nick.Kullman@gmail.com>
2016-04-11 15:05:24 -0700

    Header formatting updates

--- src/mco/alphadelta/framework/IADAlgoInterface.java
+++ src/mco/alphadelta/framework/IADAlgoInterface.java
@@ -1,12 +1,12 @@
-package mco.alphadelta.framework;
-
 /**
  * Defines the interface for interfacing with the alpha-delta algorithm.
  * 
  * @author nkullman
  * @version %I%, %G%
- * @since April 11, 2016
+ * @since Apr 11, 2016
  */
+
+package mco.alphadelta.framework;
 
 public interface IADAlgoInterface {
 
