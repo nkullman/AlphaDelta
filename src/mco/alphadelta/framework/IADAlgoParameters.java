72e4b1d3afa6eaccd3c93d8177d8ff2d464ba5ce
Nick Kullman <Nick.Kullman@gmail.com>
2016-04-11 15:06:03 -0700

    Instantiate interface for AD algo params

---
+++ src/mco/alphadelta/framework/IADAlgoParameters.java
@@ -0,0 +1,13 @@
+/**
+ * Defines the interface for the set of parameters
+ * affecting the implementation of the alpha-delta algorithm.
+ * 
+ * @author nkullman
+ * @version %I%, %G%
+ * @since Apr 11, 2016
+ */
+package mco.alphadelta.framework;
+
+public interface IADAlgoParameters {
+
+}
