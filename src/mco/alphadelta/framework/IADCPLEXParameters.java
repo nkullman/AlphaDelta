44d99bc1cd5b062c9fe1b17a190ca7de80cf3fc1
Nick Kullman <Nick.Kullman@gmail.com>
2016-04-11 15:06:27 -0700

    Instantiate interface for CPLEX params

---
+++ src/mco/alphadelta/framework/IADCPLEXParameters.java
@@ -0,0 +1,14 @@
+/**
+ * Defines the interface for the set of parameters
+ * that govern the CPLEX solver during the execution
+ * of the alpha-delta algorithm.
+ * 
+ * @author nkullman
+ * @version %I%, %G%
+ * @since Apr 11, 2016
+ */
+package mco.alphadelta.framework;
+
+public interface IADCPLEXParameters {
+
+}
