382e8af7038be2756da2b36df87dd60e56d84dd3
Nick Kullman <Nick.Kullman@gmail.com>
2016-04-12 18:23:29 -0700

    Instantiate test class for implementation of the alpha-delta algorithm

---
+++ src/mco/alphadelta/implementation/TADRun.java
@@ -0,0 +1,22 @@
+package mco.alphadelta.implementation;
+
+import mco.alphadelta.framework.IADAlgoInterface;
+
+/**
+ * Test class for implementing the alpha-delta algorithm.
+ *
+ * @author nkullman
+ * @version %I%, %G%
+ * @since Apr 12, 2016
+ */
+public class TADRun {
+
+    public static void main(String[] args) {
+        // instantiate new interface
+        IADAlgoInterface algoInterface = new ADAlgoConsoleInterface();
+        // set the parameters, model, and solver
+        algoInterface.prepAlgorithm();
+        // run
+        algoInterface.runSolver(algoInterface.getADSolver());
+    }
+}
