1bf5f95dbca32aa6e9ec1c8415759fd6b5dbaaea
Nick Kullman <Nick.Kullman@gmail.com>
2016-04-12 18:20:56 -0700

    Rename solver parameters interface and update header comments

---
+++ src/mco/alphadelta/framework/IADSolverParameters.java
@@ -0,0 +1,13 @@
+/**
+ * Defines the interface for the set of parameters that govern the solver during the execution
+ * of the alpha-delta algorithm. Examples include optimality tolerance and time limit.
+ *
+ * @author nkullman
+ * @version %I%, %G%
+ * @since Apr 11, 2016
+ */
+package mco.alphadelta.framework;
+
+public interface IADSolverParameters {
+
+}
