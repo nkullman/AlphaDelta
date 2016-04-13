f334da9f25473d813f659ea0a3919bc88ee5482c
Nick Kullman <Nick.Kullman@gmail.com>
2016-04-12 18:20:16 -0700

    Instantiate interface for a solver of the alpha-delta algorithm

---
+++ src/mco/alphadelta/framework/IADAlgoSolver.java
@@ -0,0 +1,28 @@
+/**
+ * Defines the interface for a solver for the alpha-delta algorithm.
+ *
+ * @author nkullman
+ * @version %I%, %G%
+ * @since Apr 11, 2016
+ */
+package mco.alphadelta.framework;
+
+import java.io.File;
+
+public interface IADAlgoSolver {
+    /**
+     * Solves the multi-criterion optimization model by executing the
+     * alpha-delta algorithm.
+     *
+     * @return true if the algorithm terminates successfully
+     */
+    public boolean solve(File mcoModel);
+
+    /**
+     * Set the parameters of the solver using the IADSolverParameters object passed.
+     *
+     * @param solverParameters the params to to configure the solver
+     * @return true if the parameters are set successfully.
+     */
+    public boolean setParameters(IADSolverParameters solverParameters);
+}
