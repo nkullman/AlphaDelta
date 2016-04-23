124f073b297e7d136e14f3d968e6ed30bc32baf4
Nick Kullman <Nick.Kullman@gmail.com>
2016-04-22 17:21:11 -0700

    Create  algoparams implementation with more params available

---
+++ src/mco/alphadelta/implementation/ADAlgoParameters.java
@@ -0,0 +1,68 @@
+package mco.alphadelta.implementation;
+
+import mco.alphadelta.framework.IADAlgoParameters;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by nkullman on 4/22/2016.
+ */
+public class ADAlgoParameters implements IADAlgoParameters {
+
+    private double alpha_degrees = Double.NaN;
+    private ArrayList<Double> deltas = null;
+    private boolean printLogFiles = false;
+    private boolean printSolFiles = true;
+    private boolean hotStart = false;
+    private File hotStartModel = null;
+
+    public void setAlpha_degrees(double alpha_degrees) {
+        this.alpha_degrees = alpha_degrees;
+    }
+
+    public void setDeltas(ArrayList<Double> deltas) {
+        this.deltas = deltas;
+    }
+
+    public double getAlpha_degrees() {
+        return this.alpha_degrees;
+    }
+
+    public ArrayList<Double> getDeltas() {
+        ArrayList<Double> clone = new ArrayList<>();
+
+        for (double e : this.deltas) clone.add(e);
+
+        return clone;
+    }
+
+    public void setPrintLogFiles(boolean printLogFiles) {
+        this.printLogFiles = printLogFiles;
+    }
+
+    public void setPrintSolFiles(boolean printSolFiles) {
+        this.printSolFiles = printSolFiles;
+    }
+
+    public boolean willPrintLogFiles() {
+        return printLogFiles;
+    }
+
+    public boolean willPrintSolFiles() {
+        return printSolFiles;
+    }
+
+    public boolean willHotStart() {
+        return hotStart;
+    }
+
+    public File getHotStartModel() {
+        return hotStartModel;
+    }
+
+    public void setHotStartModel(File hotStartModel) {
+        this.hotStart = true;
+        this.hotStartModel = hotStartModel;
+    }
+}
