9cc640b52755b3cfac06cbbdf7462c503593f4e5
Nick Kullman <Nick.Kullman@gmail.com>
2016-04-26 19:00:51 -0700

    Add getters for one or all CPLEX solver params

--- src/mco/alphadelta/implementation/ADSolverCPLEXParameters.java
+++ src/mco/alphadelta/implementation/ADSolverCPLEXParameters.java
@@ -4,6 +4,7 @@ import mco.alphadelta.framework.IADSolverParameters;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Nick on 4/22/2016.
@@ -13,5 +14,13 @@ public class ADSolverCPLEXParameters implements IADSolverParameters{
 
     public void addParam(String paramName, String paramValue){
         this.paramName_paramValue.put(paramName,paramValue);
+    }
+
+    public String getParam(String paramName) {
+        return this.paramName_paramValue.get(paramName);
+    }
+
+    public Set<String> getParamSet() {
+        return this.paramName_paramValue.keySet();
     }
 }
